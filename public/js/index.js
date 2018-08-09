7d51d2dbac43331032af4abac7521625d50fb48a
JINGYIDING <dingjy94@gmail.com>
2018-08-09 00:35:38 -0400

    autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 const socket = io();
 
+function scrollToBottom() {
+  const messages = $('#message');
+  const newMessage = messages.children('li:last-child');
+  //Height
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', () => {
   console.log('conneted to server');
 });
@@ -17,6 +32,7 @@ socket.on('newMessage', (message) => {
   });
 
   $('#message').append(html);
+  scrollToBottom();
 })
 
 socket.on('newLocationMessage', message => {
@@ -28,6 +44,7 @@ socket.on('newLocationMessage', message => {
   });
 
   $('#message').append(html);
+  scrollToBottom();
 });
 
 $('#messageForm').on('submit', e => {
